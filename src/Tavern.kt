95918316376241f0453f08e7ff2e512f7f327cb9
dade117 <dade117@gmail.com>
2020-09-15 22:19:57 -0500

    Chapter 11 Progress

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -10,10 +10,8 @@ import java.io.File
 
 const val TAVERN_NAME = "Taernyl's Folly"
 
-// player currency vars
-var playerGold = 10
-var playerSilver = 10
-var playerDragonCoin = 0
+// Currency vars
+val patronGold = mutableMapOf<String, Double>()
 
 val patronList = mutableListOf("Eli", "Mordoc", "Sophie")
 val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
@@ -22,6 +20,7 @@ val uniquePatrons = mutableSetOf<String>()
 val menuList = File("data/tavern-menu-data.txt").readText().split("\n")
 
 fun main() {
+    /*
     if(patronList.contains("Eli")){
         println("The tavern master says: Eli's in the back playing cards.")
     } else {
@@ -33,52 +32,36 @@ fun main() {
     } else {
         println("The tavern master says: Nay, they departed hours ago.")
     }
+     */
 
     //placeOrder("shandy,Dragon's Breath, 5.91")
     //placeOrder("elixir,Shirley's Temple,4.12")
 
+    var temp = 5
+
     (0..9).forEach{
+        temp += it
         val first = patronList.shuffled().first()
         val last = lastName.shuffled().first()
         val name = "$first $last"
         uniquePatrons += name
     }
 
-    println(uniquePatrons)
+    uniquePatrons.forEach{
+        patronGold[it] = 6.0
+    }
 
     var orderCount = 0
     while(orderCount <= 9){
         placeOrder(uniquePatrons.shuffled().first(), menuList.shuffled().first())
         orderCount++
-    }
-}
 
-fun performPurchase(price:Double): Boolean {
-    displayBalance()
-    val totalPurse = playerGold + (playerSilver / 100.0)
-    println("Total Purse: $totalPurse")
-
-    if(totalPurse >= price) {
-        println("Purchasing item for $price")
-        val remainingBalance = totalPurse - price
-        println("Remaining balance: ${"%.2f".format(remainingBalance)}")
-
-        // Logic for updating balance
-        val remainingGold = remainingBalance.toInt()
-        val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
-        playerGold = remainingGold
-        playerSilver = remainingSilver
-        displayBalance()
-
-        return true
+        if(uniquePatrons.isEmpty()){
+            break
+        }
     }
 
-    return false
-}
-
-private fun displayBalance(){
-    println("Player's purse balance:\n- Gold: $playerGold\n- Silver: $playerSilver")
-    if(playerDragonCoin > 0){ println("Dragoncoin: $playerDragonCoin") }
+    displayPatronBalances()
 }
 
 private fun placeOrder(patronName:String, menuData:String){
@@ -87,33 +70,26 @@ private fun placeOrder(patronName:String, menuData:String){
     println("$patronName speaks with $tavernMaster about their order...")
 
     val (type, name, price) = menuData.split(',')
-    val message = "$patronName buys a $name ($type) for $price."
-    println(message)
+    // val message = "$patronName buys a $name ($type) for $price."
+    // println(message)
 
-    /*
-    if(performPurchase(price.toDouble())){
-        val message = "Madrigal buys a $name ($type) for $price."
+
+    if(performPurchase(price.toDouble(), patronName)){
+        val message = "$patronName buys a $name ($type) for $price."
         println(message)
 
         val phrase = if(name == "Dragon's Breath"){
-            "Madrigal exclaims: ${toDragonSpeak("Ah delicious $name!")}"
+            "$patronName exclaims: ${toDragonSpeak("Ah delicious $name!\n")}"
         } else {
-            "Madrigal says: Thanks for the $name."
+            "$patronName says: Thanks for the $name.\n"
         }
 
         println(phrase)
     } else {
-        println("Bartender says: You don't have enough money friend! Come back when you do")
+        println("Bartender says: You don't have enough money for $name, $patronName!")
+        println("Bartender says: Bouncer! Show $patronName where the peasants\nwith no gold in their pockets go!")
+        tavernBounce(patronName)
     }
-     */
-
-    val phrase = if(name == "Dragon's Breath"){
-        "$patronName exclaims: ${toDragonSpeak("Ah delicious $name!")}"
-    } else {
-        "$patronName says: Thanks for the $name."
-    }
-
-    println(phrase)
 }
 
 private fun toDragonSpeak(phrase:String)=
@@ -160,7 +136,7 @@ fun printMenu(){
     for(item in shandy){
         val(_,name,price) = item.split(",")
 
-        var numDots = 20 + (18 - name.length)
+        val numDots = 20 + (18 - name.length)
         println("$name${".".repeat(numDots)}$price")
     }
 
@@ -168,7 +144,7 @@ fun printMenu(){
     for(item in elixir){
         val(_,name,price) = item.split(",")
 
-        var numDots = 20 + (18 - name.length)
+        val numDots = 20 + (18 - name.length)
         println("$name${".".repeat(numDots)}$price")
     }
 
@@ -176,7 +152,7 @@ fun printMenu(){
     for(item in meal){
         val(_,name,price) = item.split(",")
 
-        var numDots = 20 + (18 - name.length)
+        val numDots = 20 + (18 - name.length)
         println("$name${".".repeat(numDots)}$price")
     }
 
@@ -184,8 +160,29 @@ fun printMenu(){
     for(item in desertDessert){
         val(_,name,price) = item.split(",")
 
-        var numDots = 20 + (18 - name.length)
+        val numDots = 20 + (18 - name.length)
         println("$name${".".repeat(numDots)}$price")
     }
+}
+
+fun performPurchase(price:Double, patronName:String): Boolean {
+    val totalPurse = patronGold.getValue(patronName)
+    if(totalPurse >= price){
+        patronGold[patronName] = totalPurse - price
+    } else { return false }
+
+    return true
+}
+
+private fun displayPatronBalances(){
+    patronGold.forEach{patron, balance ->
+        println("$patron balance: ${"%.2f".format(balance)}")
+    }
+}
+
+private fun tavernBounce(patronName: String){
+    uniquePatrons.remove(patronName)
+    patronGold.remove(patronName)
+    println("$patronName is shown to the door by the bouncer.\n")
 }
 
